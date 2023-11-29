93af8384b5b055bd36612de701356b64e7b46f64
ravi_thokal <>
2023-11-29 15:19:39 +0530

    TMDB Api Changes

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constants'
-
+import {useSelector } from 'react-redux'
+import useMovieTrailer from '../hooks/useMovieTrailer'
 const VideoBackground = ({movieId}) => {
-    const getMovieVideos = async () =>{
-        const data = await fetch('https://api.themoviedb.org/3/movie/901362/videos?language=en-US', API_OPTIONS);
-        const json = await data.json();
-        console.log(json.results)
-    }
+    const trailerV = useSelector( store => store.movies?.trailerVideo);
 
-    useEffect(() =>{
-        getMovieVideos();
-    },[])
+   useMovieTrailer(movieId)
+  
   return (
     <div>
-      
+      <iframe className='w-screen aspect-video'  src={"https://www.youtube.com/embed/"+ trailerV.key + '?&autoplay=1&mute=1'} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </div>
   )
 }
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='pt-24 px-12'>
+    <div className='w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-5xl font-bold'>{title}</h1>
         <p className='py-6 text-lg w-1/2'>{overview}</p>
-        <div className='p'>
-            <button className='bg-gray-500 text-white px-12 p-4 text-xl opacity-25 rounded-lg'>📨  Play</button>
-            <button className='mx-3 bg-gray-500 text-white px-12 p-4 text-xl opacity-25 rounded-lg'>More Info</button>
+        <div className=' text-white'>
+            <button className='bg-white text-black px-12 p-4 text-xl rounded-lg hover:bg-opacity-80'>Play</button>
+            <button className='mx-3 bg-gray-500 text-white px-12 p-4 text-xl bg-opacity-50 rounded-lg'> More Info</button>
         </div>
       
     </div>
